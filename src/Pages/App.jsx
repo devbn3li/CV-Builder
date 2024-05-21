02fbdb9c6fac56124a0e3ea466700367f1ccec2d
devbn3li <fudwhc@gmail.com>
2024-05-21 18:28:29 +0300

    Add Navbar component

--- src/Pages/App.jsx
+++ src/Pages/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import Navbar from "../Components/Navbar";
 
 const App = () => {
   return (
     <div>
-      <h1>CV Builder</h1>
+      <Navbar />
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import '../public/styles/index.css'
+import App from './Pages/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    
+    <App />
   </React.StrictMode>,
 )
