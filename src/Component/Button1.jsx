f8ec6918be233c4612d48676d80072141666b34a
devbn3li <fudwhc@gmail.com>
2024-05-29 17:53:27 +0300

    Enhancments

---
+++ src/Component/Button1.jsx
@@ -0,0 +1,11 @@
+const DownloadBtn = (props) => {
+  return (
+    <button
+      className={`w-12 aspect-square rounded-full text-xl flex justify-center items-center hover:text-primary-100 text-white bg-transparent opacity-70  hover:bg-gray-700/40  hover:opacity-90 transition-all hover:delay-0 delay-300 ${props.classes}`}
+      onClick={props.ClickFunc}
+    >
+      <props.Icon />
+    </button>
+  );
+};
+export default DownloadBtn;
